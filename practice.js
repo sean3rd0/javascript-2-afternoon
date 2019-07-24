9645c4d1fb904d317f6800a5cc4a176f9401c194
Shannon Scott <SeanScott@Seans-MacBook-Pro-6.local>
2019-07-24 15:51:08 -0600

    Finished Javascript stuff

--- practice.js
+++ practice.js
@@ -14,7 +14,9 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+//Code here 
+let me = {name : 'Sean', age : 24} 
+me.name
 
 ////////// PROBLEM 2 //////////
 
@@ -24,20 +26,26 @@
 */
 
 //Code here
-
+var favoriteThings = {band : 'Nashville Tribute', food : "oatmeal", person : "Jesus Christ", book : "The Book of Mormon", movie : "Ben Hur", holiday : "Easter"};
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
-  and then another key named 'brand' with the value being your favorite brand.
+  and then another key named 'brand' with the value being your favorite brand. 
 */
 
 //Code here
-
+let favoriteBrand = "Ferrari" 
+let favoriteCar = "v10Formula1"
+favoriteThings.car = favoriteCar 
+favoriteThings.brand = favoriteBrand 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
 //Code here
+favoriteThings['food'] = 'Chicken Nuggets'
+favoriteThings.book = "Harry Potter"
+
 
 ////////// PROBLEM 3 //////////
 
@@ -50,13 +58,16 @@
 */
 
 //Code here
-
+let backPack = {}
+let item = 'firstPocket';
+backPack[item] = 'chapstick';
+backPack.color = "Pink"
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
-
+alert (backPack)
 /*
 You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
@@ -64,7 +75,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
-
+console.log backpack
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
@@ -85,7 +96,8 @@ var user2 = {
 */
 
 //Code Here
-
+user2.name = 'Bryan G. Smith'
+user2.email = 'bryan.smith@devmounta.in'
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
@@ -96,14 +108,14 @@ var user2 = {
 */
 
 //Code Here
-
+let methodCollection = {}
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
 */
 
 //Code Here
-
+methodCollection = {[function alertHello = alert ('hello')], [function logHello = log ('hello')]}
 /*
   Now call your alertHello and logHello methods.
 */
